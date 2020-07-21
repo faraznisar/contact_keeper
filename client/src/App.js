bdc081e9a77240b250d1c5d31261f8cfffbb75e7
Faraz Shah <faraz.nisar1@gmail.com>
2020-07-21 23:44:08 +0530

    Client setup with Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>Client</p>
-    </div>
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
